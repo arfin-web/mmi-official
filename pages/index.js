f935a8e5ff43106eabb9758a64b3e90453967832
arfin-web <arfinpriom12@gmail.com>
2022-09-02 10:40:26 -0700

    seo updated

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ export default function Home() {
       <Head>
         <title>Multilateral Model Institute</title>
         <meta name="description" content="Multilateral Model Institute. Address: Road No - 1, House No - 13, Block - A, Dhaka Uddan, Mohammadpur, 1207" />
+        <meta name="keywords" content="Multilateral Model Institute, mmi, mmi offiial" />
+        <meta name="author" content="Multilateral Model Institute" />
         <meta name="robots" content="index, follow" />
         <meta property="og:title" content="Multilateral Model Institute" />
         <link rel="icon" href="/favicon.ico" />
